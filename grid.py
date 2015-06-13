c823876b5519e040f9afc6c6be6e5ca30aaf2cca
rirze <cheedella.sourabh@gmail.com>
2015-06-12 21:09:05 -0400

    Sectioned main logic into functions (grid.py) and fully vectorized triangle finding algorithm (trinterior.py)

--- grid.py
+++ grid.py
@@ -3,27 +3,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Delaunay 
+import scipy.optimize as op
 import random
 
 import trinterior as trint
 
-b = 1 # radius of magnification function
-
-def f(x):
+def f(x,b):
     '''our magnification function'''
     return np.sqrt(b**2-x**2)
     
-def nf(x):
+def nf(x,b):
     '''our magnification function (negative y-axis values)'''
     return -1* np.sqrt(b**2-x**2)
     
-def carmapping(x,y):
+def mapping(v,u,b):
+    w,z = u #image
+    x,y = v #actual position we want to find
+    return [x - b*x/np.sqrt(x**2 + y**2) - w, y - b*y/np.sqrt(x**2 + y **2) - z]
+
+def carmapping(x,y,b):
     '''mapping of cartesian coordinates from image to source plane'''
     x = np.array(x)
     y = np.array(y)
     return np.transpose([x - b*x/np.sqrt(x**2 + y**2), y - b*y/np.sqrt(x**2 + y **2)])
 
-def polmapping(r,th):
+def polmapping(r,th,b):
     '''mapping of polar coordinates from image to source plane'''
     r  = np.array(r)
     th = np.array(th)
@@ -37,132 +41,216 @@ def polartocar(r,th):
             
     return np.transpose([r*np.cos(th),r*np.sin(th)])
 
-def condition(x,y,coord):
+def distance(x,y,coord,b):
     '''returns the distance between the critical curve and the point '''
     if coord == 'car':
         return np.sqrt(x**2 + y**2) - b
     elif coord == 'pol':
         return x - b
     else:
-        return NaN
+        raise NameError('Unrecogonized Coordinate System')
 
-def relation(x,y,coord):
+def relation(x,y,coord,b):
     '''tells us if the point pair (x,y) is outside, inside, or on the critical curve'''
-    dif = condition(x,y,coord)
+    dif = distance(x,y,coord,b)
     return np.sign(dif) # -1 for inside, +1 for outside, 0 for exactly on
 
-def buildrelations(xran,yran,coord):
+def buildrelations(xran,yran,coord,b):
     '''applies relation() on 2D range specified by xran and yran. Returns a 2D array.'''
     xx,yy = np.meshgrid(xran,yran,sparse=True)
+    return np.transpose(relation(xx,yy,coord,b))
     
-    return np.transpose(relation(xx,yy,coord))
-    
-def changep(fir,sec,thr,frt):
+def mag_change(fir,sec,thr,frt):
     '''[[fir, sec],[thr,frt]] - returns true if there is a change in magnification in the box specified'''
     return (fir*sec < 0 or sec*frt < 0 or frt*thr < 0 or thr*fir < 0)
 
 RECURSEDEPTH = 4 #just here for future purpose when we want to set the max depth
 
-def points(stack,xran,yran,coord,n=0):
+def points(stack,xran,yran,coord,b,n=0):
     '''collects the points around our magnification function and appends them to the 'stack'. Subdivides grid by 2 in each recursion.'''
-    mat = buildrelations(xran,yran,coord)
+    mat = buildrelations(xran,yran,coord,b)
     xlen = len(xran)
     ylen = len(yran)
     for i in range(xlen-1):
         for j in range(ylen-1):
-            if n != RECURSEDEPTH and changep(mat[i][j],mat[i][j+1],mat[i+1][j],mat[i+1][j+1]):
+            if n != RECURSEDEPTH and mag_change(mat[i][j],mat[i][j+1],mat[i+1][j],mat[i+1][j+1]):
                 points(stack,
                        np.linspace(xran[i],xran[i+1],3,endpoint=True),
                        np.linspace(yran[j],yran[j+1],3,endpoint=True),
-                       coord,n+1)
+                       coord,b,n+1)
             else:
                 stack.append([xran[i+1],yran[j]])
                 stack.append([xran[i+1],yran[j+1]])
                 stack.append([xran[i],yran[j]])
                 stack.append([xran[i],yran[j+1]])
 
-##### SCRIPT BEGINS ######
+def neg_mag(x,y):
+    r = np.sqrt(x**2+y**2)
+    mag = 1/(1-1/r)
+    return mag <= 0
+
     
-# plot our function, 200 points
-funx = np.linspace(-1,1,200,endpoint=False)
-funy1 = f(funx)
-funy2 = nf(funx) 
+def mag_limit(x,y,b):
+    r = np.sqrt(x**2+y**2)
+    mag = r/(r-b)
+    return mag > 3 or mag < -2
+    
+def sprinkle_points(stack,xran,yran,num):
+    xmin, xmax = xran
+    ymin, ymax = yran
+    for i in range(num):
+        stack.append(
+            (random.uniform(xmin,xmax), random.uniform(ymin,ymax)))
 
-## critical curves
-critx = np.hstack((funx,funx[::-1])) 
-crity = np.hstack((funy1,funy2[::-1])) #here and above the second pairs are put in reverse order so the line connecting the points makes a nice circe rather than crossing the origin to start from the other side
 
-critpairs = zip(critx,crity)
+    
+                         
+def points2(stack,xran,yran,spacing,b):
+    xs = np.tile(xran,len(yran))
+    ys = np.repeat(yran,len(xran))
+    pairs = zip(xs,ys)
+    sides,neg_sides = 3,7
+    width,neg_width = spacing/sides,spacing/neg_sides
+    
+    for i in pairs:
+        x,y = i
+        #if neg_mag(x,y):
+         #   sprinkle_points(stack,(x-spacing/2.,x+spacing/2.),(y-spacing/2.,y+spacing/2.),200)
+        if mag_limit(x,y,b):
+            sprinkle_points(stack,(x-spacing/2.,x+spacing/2.),(y-spacing/2.,y+spacing/2.),100)
+            
+    stack += pairs
+        
+        
+def generate_ranges(critargs,carargs,polargs):
+    # generate our critical curve function, 200 points
+    b, numpoints = critargs
+    funx = np.linspace(-b,b,numpoints,endpoint=False)
+    funy1 = f(funx,b)
+    funy2 = nf(funx,b) 
 
-## caustics?
+    ## critical curves
+    critx = np.hstack((funx,funx[::-1])) 
+    crity = np.hstack((funy1,funy2[::-1])) #here and above the second pairs are put in reverse order so the line connecting the points makes a nice circe rather than crossing the origin to start from the other side
 
-transcrit = carmapping(critx,crity)
+    ## caustics?
 
-# initial cartesian grid, coarse,
-spacing = 0.5
-upperend = 2.5
-lowerend = -upperend
-x = np.arange(lowerend,upperend+spacing,spacing)
-y1 = np.arange(0,upperend+spacing,spacing)
-y2 = np.arange(lowerend,spacing,spacing)
+    caustics = carmapping(critx,crity,b)
 
-#intial polar grid
-rspacing = 0.03
-rupper = 1.2
-thetadivisions = 42
-r = np.arange(0,rupper+rspacing,rspacing)
-theta = np.linspace(0,2*np.pi,thetadivisions)
+    # initial cartesian grid, coarse,
+    lowerend, upperend, spacing = carargs
+    #spacing = 0.5
+    #upperend = 2.5
+    #lowerend = -upperend
+    x = np.arange(lowerend,upperend+spacing,spacing)
+    y = np.arange(lowerend,upperend+spacing,spacing)
 
-## the rs and thetas formed from a cartesian product. Used for vector operations.
-rs = np.tile(r,len(theta))
-thetas = np.repeat(theta,len(r))
+    #intial polar grid
+    
+    rupper, rspacing, thetadivision = polargs
+    #rspacing = 0.03
+    #rupper = 0.8 #rspacing*10
+    #thetadivision = 42
+    thetaspacing = 2*np.pi/thetadivision
+    r = np.arange(0,rupper+rspacing,rspacing)
+    theta = np.arange(0,2*np.pi,thetaspacing)
 
-#polstack = [] # stack for holding the polar points #needed if we subgrid on polar grids
-carstack = [] # stack for holding the cartesian points
+    ## the rs and thetas formed from a cartesian product. Used for vector operations.
+    rs = np.tile(r,len(theta))
+    thetas = np.repeat(theta,len(r))
 
-# SUBGRIDDING!!!!
-#points(polstack,r,theta,'pol') # if we wanted to subgrid on the polar grid, uncomment
-points(carstack,x,y1,'car') #generate subgrid on cartesian grid on for the +y axis range
-points(carstack,x,y2,'car') #generate subgrid on cartesian grid on for the -y axis range
+    return [ [critx, crity], caustics, [x,y], [r, theta], [rs, thetas]]
 
-carstack = np.array(carstack) 
-polstack = np.array(polartocar(rs,thetas))
-stack = np.concatenate((carstack,polstack),axis=0) #combine list of cartesian and polar pairs
-transformed = np.array(carmapping(*np.transpose(stack))) #transform pairs from image to source plane
 
-dpoints = Delaunay(stack) # generate Delaunay object for triangulization/triangles
 
-# finding source positions
-image = (random.random()*2-1,random.random()*2-1) #a random coordinate pair with x,y chosen from -1:1
+def transformations(car_ranges, pol_ranges, spacing, b):
 
-lenstri = transformed[dpoints.simplices.copy()] #triangles on the source plane
-imagetri= stack[dpoints.simplices.copy()] #triangles on the image plane
+    x,y = car_ranges
+    r,theta = pol_ranges
+    
+    polstack = [] # stack for holding the polar points #needed if we subgrid on polar grids
+    carstack = [] # stack for holding the cartesian points
+    stack = []
+    #points(polstack,r,theta,'pol') # if we wanted to subgrid on the polar grid, uncomment
+    #points(carstack,x,y,'car',b) #generate subgrid on cartesian grid
+    points2(carstack,x,y,spacing,b)
+    stack = np.array(stack)
+    carstack = np.array(carstack) 
+    polstack = np.array(polartocar(r,theta)) #comment if we subgrid on polar grids
+    stack = np.concatenate((carstack,polstack),axis=0) #combine list of cartesian and polar pairs
+    transformed = np.array(carmapping(stack[:,0],stack[:,1],b)) #transform pairs from image to source plane
 
-indices = trint.find(image,lenstri) #list of which triangles contain point on source plane
-sourcetri = imagetri[indices] 
-sourcepos = np.sum(sourcetri,axis=1)/3.0 #list of the centroid coordinates for the triangles which contain the point 'image'
+    dpoints = Delaunay(stack) # generate Delaunay object for triangulization/triangles
 
-# plot them all
+    return [stack, transformed, dpoints]
 
-plt.subplot(1,2,1) # image plane
-plt.title('Image Plane')
-plt.axis([lowerend,upperend,lowerend,upperend])
-plt.gca().set_aspect('equal', adjustable='box') # equal ratios on x and y axis
 
-plt.plot(critx,crity,zorder=2) # plot of critical curve(s)
 
-plt.triplot(stack[:,0],stack[:,1], dpoints.simplices.copy(),zorder=1) # plot of the Delaunay Triangulization
+def find_source(stack, transformed, simplices, image_loc,b):
+    lenstri = transformed[simplices] #triangles on the source plane
+    imagetri= stack[simplices] #triangles on the image plane
 
-plt.scatter(*zip(*sourcepos), marker='*', color='black',s=100, zorder=2) # plot of the (approximate/centroid) positions of the image
+    indices = trint.find2(image_loc,lenstri.copy()) #list of which triangles contain point on source plane
 
-plt.subplot(1,2,2) # source plane
-plt.title('Source Plane')
-plt.gca().set_aspect('equal', adjustable='box') # equal ratios on x and y axis
+    sourcetri = imagetri[indices] 
+    sourcepos = np.sum(sourcetri.copy(),axis=1)/3.0 #list of the centroid coordinates for the triangles which contain the point 'image'
+    realpos = np.array([(op.root(lambda x0: mapping(x0,image_loc,b),v)).x for v in sourcepos]) # use centroid coordinates as guesses for the actual root finding algorithm
 
-plt.triplot(transformed[:,0],transformed[:,1], dpoints.simplices.copy(), zorder=1) # plot of the transformed Delaunay Triangulization
+    return [sourcepos, realpos]
 
-plt.scatter(*zip(image), marker='*', color='red', s= 200, zorder=2 ) # plot of (observed) image position
+def plot_graphs(critx,crity,caustics,stack,transformed,simplices,sourcepos,realpos,image,lowerend,upperend):
+    plt.subplot(1,2,1) # image plane
+    plt.title('Image Plane')
+    plt.axis([lowerend,upperend,lowerend,upperend])
+    plt.gca().set_aspect('equal', adjustable='box') # equal ratios on x and y axis
+    
+    plt.plot(critx,crity,zorder=2) # plot of critical curve(s)
 
-plt.plot(transcrit[:,0],transcrit[:,1], color ='green', zorder=2) # plot of caustics?
-plt.show()
+    plt.triplot(stack[:,0],stack[:,1], simplices,zorder=1) # plot of the Delaunay Triangulization
+
+    plt.scatter(*zip(*sourcepos), marker='*', color='black',s=100, zorder=2) # plot of the (approximate/centroid) positions of the image
+    plt.scatter(*zip(*realpos), marker='*', color='purple', s=100, zorder=2)
+
+    plt.subplot(1,2,2) # source plane
+    plt.title('Source Plane')
+    plt.gca().set_aspect('equal', adjustable='box') # equal ratios on x and y axis
+
+    plt.triplot(transformed[:,0],transformed[:,1], simplices, zorder=1) # plot of the transformed Delaunay Triangulization
+
+    plt.scatter(*zip(image), marker='*', color='red', s= 200, zorder=2 ) # plot of (observed) image position
+
+    plt.plot(caustics[:,0],caustics[:,1], color ='green', zorder=2) # plot of caustics?
+    plt.show()
+
+
+def run(critargs,carargs,polargs,show_plot=True,image=(random.random()*2-1,random.random()*2-1)):
+    b = critargs[0]
+    lowerend, upperend, spacing = carargs
+    
+    args = generate_ranges(critargs,carargs,polargs)
+    critx, crity = args[0]
+    caustics = args[1]
+    x,y = args[2]
+    r,theta = args[3]
+    rs,thetas = args[4]
+
+    stack, transformed, dpoints = transformations((x,y),(rs,thetas),spacing, b)
+    
+    #image = (random.random()*2-1,random.random()*2-1) #a random coordinate pair with x,y chosen from -1:1
+
+    sourcepos, realpos = find_source(stack, transformed, dpoints.simplices.copy(), image, b)
+
+
+
+    if show_plot:
+        plot_graphs(critx,crity,caustics,stack,transformed,dpoints.simplices.copy(),sourcepos,realpos,image,lowerend,upperend)
+
+critargs = [1.,200] #critical curve radius, number of points on critical curve function
+carargs = [-2.5,2.5,0.5] # lower bound, upper bound, initial spacing (all 3 quantities apply the same to x and y axes)
+polargs = [0.3,0.03,42] # outer radius, radius spacing, number of divisions in angle (for 360 degrees)
+image = [0.6,-0.1] #image location if we want to specify
+
+run(critargs,carargs,polargs)
+
+# [1,200], [-2.5,2.5,0.5], [0.3,0.03,42] # sample runtime parameters
 
--- trinterior.py
+++ trinterior.py
@@ -1,62 +1,83 @@
 #!/usr/bin/python
 
 import numpy as np
-
-
-def dot(u,v):
-    '''Dot Product of two vectors'''
-    return u[0]*v[0] + u[1]*v[1]
+import numpy.linalg as la
 
 def sub(u,v):
     '''Subtract two vectors '''
     return (u[0]-v[0],u[1]-v[1])
 
-def condition(P, tri):
-    '''Returns a Boolean whether P is inside the triangle defined by vertices A,B,C'''
-    # Code shamelessly copied from http://www.blackpawn.com/texts/pointinpoly/
-    # Uses barycentric coordinates to find whether the point is inside the triangle
-    (A,B,C) = tri
-    
+def det(u,v):
+    '''Return the determinant of the 2-D matrix formed by u and v'''
+    return u[0]*v[1]-u[1]*v[0]
 
+def inside_triangle(P,tri):
+    '''Explicit expression of the components in the algebra of finding barycentric coordiantes. Plugging in an x and y value for v0,v1,v2 reduces the computation to three determinants from before which computed six dot products.'''
+    # Code derived from http://www.blackpawn.com/texts/pointinpoly/
+    # Uses barycentric coordinates to find whether the point is inside the triangle
+
+    A,B,C = tri
+    
     v0 = sub(C,A)
     v1 = sub(B,A)
     v2 = sub(P,A)
-    
-    dot00 = dot(v0,v0)
-    dot01 = dot(v0,v1)
-    dot02 = dot(v0,v2)
-    dot11 = dot(v1,v1)
-    dot12 = dot(v1,v2)
-    
-    denom = (dot00 * dot11 - dot01 * dot01)
-    if denom == 0: return False
-    
-    u = (dot11 * dot02 - dot01 * dot12) / denom
-    v = (dot00 * dot12 - dot01 * dot02) / denom
-    
-    return u >= 0 and v >= 0 and u + v < 1
 
+    invDenom = 1.0/det(v1,v0)
+    
+    u = det(v2,v1) * (-invDenom)
+    v = det(v2,v0) *   invDenom
+
+    return u >= 0 and v >= 0 and u + v < 1
 
 def find(v,triangles):
     '''Given a point v and a list of triangles defined by three verticies, this function will return the indicies of the triangles in which the point is inside the triangle.'''
-    length = len(triangles)
-    indices = []
-    for i in range(length):
-        if condition(v,triangles[i]):
-            indices.append(i)
+
+    indices = [i for i,triangle in enumerate(triangles) if inside_triangle(v,triangle)]
+            
+    return indices
+
+def np_inside_triangle(A,B,C,P):
+    '''Explicit expression of the components in the algebra of finding barycentric coordiantes. Plugging in an x and y value for v0,v1,v2 reduces the computation to three determinants from before which computed six dot products. This particular function uses numpy vector operations, where the inputs are vectors of pairs. 'a,b,c' are the lists of pairs of the triangle vertices, and p is still a single point for which we are searching for.'''
+    # Code derived from http://www.blackpawn.com/texts/pointinpoly/
+    # Uses barycentric coordinates to find whether the point is inside the triangle
+    # Uses numpy vector operations
+
+    v0 = C-A
+    v1 = B-A
+    v2 = P-A
+    
+    with np.errstate(divide='ignore',invalid='ignore'): 
+        #my understanding is that invalid values arise from floating point errors, so nothing to worry about? still gives good results...
+        invDenom = 1.0/la.det(np.transpose(np.dstack((v1,v0)),[0,2,1])) #divide/0 
+    
+        u = la.det(np.transpose(np.dstack((v2,v1)),[0,2,1])) * (-invDenom) #invalid value in det()
+        v = la.det(np.transpose(np.dstack((v2,v0)),[0,2,1])) *   invDenom  #invalid value in det() 
+
+        coords = np.vstack((u,v,1-u-v))
+        indices = np.nonzero(np.all(coords>=0,axis=0)) #invalid value in greater_equal
     
     return indices
-        
+
+def find2(p,triangles):
+    '''Wrapper for the algorithm that uses numpy vector operations. Similiar to the 'find()' wrapper. Use this one for better speed!'''
+    a,b,c = np.transpose(triangles,[1,0,2])
+    return np_inside_triangle(a,b,c,p)
+
+
 ### the following code is for testing purposes ###
 
 
 sampletriangles = np.load("triangles.npy") #load a working example of a list of triangles formed by Delaunay Triangulization, for use in the test function below
 def test():
-    '''a function used to time the 'condition' algorithm'''
-    find([0.5,0.5],sampletriangles)
+    '''a function used to time the 'inside_triangle' algorithm'''
+    find2([0.5,0.5],sampletriangles)
 
 # run from the command line to time the function:
 ## python -m timeit -v -s'import trinterior' 'trinterior.test()'
 
+# Speedups
+# dot products -> determinants in inside_triangle(): 80 msec to 60 msec
+# for loop -> list comprehension in find(): 60 msec to 56 msec
+# find() -> find2() -- full transition to numpy vector operations: 56 msec to 15 msec
 
     
