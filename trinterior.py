1760f644fe945688a43809df1ff86ff491a223f3
rirze <cheedella.sourabh@gmail.com>
2015-06-27 09:40:41 -0400

    Replaced numpy det() with custom function for 2D determinants.

--- trinterior.py
+++ trinterior.py
@@ -36,6 +36,12 @@ def find(v,triangles):
             
     return indices
 
+def custom_2D_det(u,v):
+    u1,u2 = u.T
+    v1,v2 = v.T
+    return u1*v2-u2*v1
+
+    
 def np_inside_triangle(A,B,C,P):
     '''Explicit expression of the components in the algebra of finding barycentric coordiantes. Plugging in an x and y value for v0,v1,v2 reduces the computation to three determinants from before which computed six dot products. This particular function uses numpy vector operations, where the inputs are vectors of pairs. 'a,b,c' are the lists of pairs of the triangle vertices, and p is still a single point for which we are searching for.'''
     # Code derived from http://www.blackpawn.com/texts/pointinpoly/
@@ -48,11 +54,11 @@ def np_inside_triangle(A,B,C,P):
     
     with np.errstate(divide='ignore',invalid='ignore'): 
         #my understanding is that invalid values arise from floating point errors, so nothing to worry about? still gives good results...
-        invDenom = 1.0/la.det(np.transpose(np.dstack((v1,v0)),[0,2,1])) #divide/0 
+        invDenom = 1.0/custom_2D_det(v1,v0) #divide/0 
     
-        u = la.det(np.transpose(np.dstack((v2,v1)),[0,2,1])) * (-invDenom) #invalid value in det()
-        v = la.det(np.transpose(np.dstack((v2,v0)),[0,2,1])) *   invDenom  #invalid value in det() 
-
+        u = custom_2D_det(v2,v1) * (-invDenom) #invalid value in det
+        v = custom_2D_det(v2,v0) *   invDenom  #invalid value in det
+        
         coords = np.vstack((u,v,1-u-v))
         indices = np.nonzero(np.all(coords>=0,axis=0)) #invalid value in greater_equal
     
@@ -63,26 +69,6 @@ def find2(p,triangles):
     a,b,c = np.transpose(triangles,[1,0,2])
     return np_inside_triangle(a,b,c,p)
 
-def np_inside_triangle_dots(A,B,C,P):
-    v0 = C-A
-    v1 = B-A
-    v2 = P-A
-
-    dot00 = np.dot(v0, v0)
-    dot01 = np.dot(v0, v1)
-    dot02 = np.dot(v0, v2)
-    dot11 = np.dot(v1, v1)
-    dot12 = np.dot(v1, v2)
-
-    invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
-    u = (dot11 * dot02 - dot01 * dot12) * invDenom
-    v = (dot00 * dot12 - dot01 * dot02) * invDenom
-
-    coords = np.vstack((u,v,1-u-v))
-    indices = np.nonzero(np.all(coords>=0,axis=0)) #invalid value in greater_equal
-    
-    return indices
-    
 ### the following code is for testing purposes ###
 
 
@@ -98,5 +84,6 @@ def test():
 # dot products -> determinants in inside_triangle(): 80 msec to 60 msec
 # for loop -> list comprehension in find(): 60 msec to 56 msec
 # find() -> find2() -- full transition to numpy vector operations: 56 msec to 15 msec
+# np.det() -> custom_2D_det(): 15 msec to 1 msec 
 
     
