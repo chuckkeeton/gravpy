e453de5d44ba888f46031eff7cf7dc5dcada48a3
ropc <itslikeroar2412@gmail.com>
2016-03-30 03:03:02 -0400

    changed values according to Sourabh's suggestion

--- example_run.py
+++ example_run.py
@@ -13,7 +13,7 @@ pmodelargs = [
     # SIE(0.5, 1.5, 1.5, 0, 0.2, 0.0),
     # SIE(0.1, 0.25, 0.25, 0.1, 0, 0),
     # SIE(0.3, -0.4, 0.6, 0.5, 45, 0),
-    NFW(0.5, 0, 0, 0, 0, 0.5, 1, 1)
+    NFW(2, 0, 0, 0, 0, 0.5, 1, 0.5)
 ]
 
 # center position (coordinate pair), outer radius, number of divisions in radius,
